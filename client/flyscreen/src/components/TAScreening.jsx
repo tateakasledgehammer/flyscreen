330cb1462e9a668029cc3d487ada52aea52562fe
tateakasledgehammer <tatewilliams29@gmail.com>
2025-08-26 21:39:38 +0930

    added utils to assist with functions for screening at different stages

--- client/flyscreen/src/components/TAScreening.jsx
+++ client/flyscreen/src/components/TAScreening.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from "react"
 import StudyCard from "./StudyCard";
+import { handleSortByOrder } from "../utils/screeningTools";
 
 export default function TAScreening(props) {
     const { studies, setStudies, savedStudies, toggleDetails, setToggleDetails } = props;
@@ -16,33 +17,6 @@ export default function TAScreening(props) {
         setItemsPerPage(e.target.value);
         setCurrentPage(1);
     }
-    
-    function handleSortByOrder(studies) {
-        return [...studies].sort((a, b) => {
-            switch (sortBy) {
-                case 'year_asc':
-                    return (a.year - b.year);
-                case 'year_des':
-                    return (b.year - a.year);
-                case 'title_asc':
-                    return a.title.localeCompare(b.title);
-                case 'title_des':
-                    return b.title.localeCompare(a.title);
-                case 'author_asc':
-                    return a.authors.localeCompare(b.authors);
-                case 'author_des':
-                    return b.authors.localeCompare(a.authors);
-                case 'index_asc':
-                    return a.id.localeCompare(b.id)
-                case 'probability_asc':
-                    return 0;
-                case 'probability_des':
-                    return 0;
-                default:
-                    return 0;
-            }
-        });
-    }
 
     useEffect(() => {
         setStudies(prev => handleSortByOrder(prev, sortBy));
@@ -52,8 +26,6 @@ export default function TAScreening(props) {
         setSearchFilter(searchFilterInput);
         setCurrentPage(1);
         setSearchFilterInput("");
-
-        
     }
 
     function handleRemoveSearchFilter() {
